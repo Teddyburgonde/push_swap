13247ae3f849e90dd8bf796eb64c90ee864d97c8
Teddy Bandama <tebandam@2G1.42angouleme.fr>
2024-01-01 17:28:15 +0100

    ra ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 12:59:13 by tebandam          #+#    #+#             */
-/*   Updated: 2024/01/01 16:57:23 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/01/01 17:28:03 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,21 @@
 #include "./libft/includes/libft.h"
 #include "./push_swap.h"
 
-//ra (rotate a) : Décale d’une position vers le haut tous les élements de la pile a.
-// Le premier élément devient le dernier.
-// int	ft_push(t_list **stack_dst, t_list **stack_src, char c)
-// {
-// 	t_list	*head;
-
-// 	if (ft_lstsize(*stack_dst) == 0
-// 		|| ft_lstsize(*stack_src) == 0)
-// 		return (-1);
-// 	head = *stack_src;
-// 	*stack_src = (*stack_src)->next;
-// 	ft_lstadd_front(stack_dst, head);
-// 	ft_printf("p%c\n", c);
-// 	return (0);
-// }
-
 void	ft_rotate(t_list **stack, char c)
 {
-	//t_list *head;
-	// t_list	*tmp;
-
-	// tmp = *stack;
-	// while ((*stack)->next != NULL)
-	// {
-	// 	stack = &(*stack)->next;
-	// 	if ((*stack)->next == NULL)
-	// 	{
-	// 		(*stack)->next = tmp;
-	// 	}
-	// }
-	// tmp->next = NULL;
-	// ft_printf("r%c\n", c);
-		t_list *tmp = *stack;
-        *stack = (*stack)->next;
-
-        t_list *current = *stack;
-        while (current->next)
-        {
-            current = current->next;
-        }
-
-        tmp->next = NULL;
-        current->next = tmp;
-
-        ft_printf("r%c\n", c);
-
+	t_list *tmp;
+	t_list *current;
+	
+	tmp = *stack;
+	*stack = (*stack)->next;
+	current  = *stack;
+	while (current->next)
+	{
+		current = current->next;
+	}
+	tmp->next = NULL;
+	current->next = tmp;
+	ft_printf("r%c\n", c);
 }
 
 
